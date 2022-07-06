29db51adc2c3ed026786f3a7c1a6f4d12434eed8
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-07-06 21:15:01 +0000

    Add index_base.html template to fix home page, and fix settings warning

--- power_standz/settings.py
+++ power_standz/settings.py
@@ -169,6 +169,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
