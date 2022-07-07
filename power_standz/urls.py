6ec55b154924f4b43c39449caebc8c1a981c8521
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-07-07 17:37:37 +0000

    Change home page url path to products page

--- power_standz/urls.py
+++ power_standz/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('', include('products.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
