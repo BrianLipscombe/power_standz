527ebeb66b61612e3da43b8dff6d0180f03d1be1
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-07-17 18:14:47 +0000

    Remove collectstatic var to fix deployment error

--- power_standz/settings.py
+++ power_standz/settings.py
@@ -18,13 +18,10 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['powerstandz.herokuapp.com', 'localhost']
