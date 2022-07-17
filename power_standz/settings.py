87681f4524da24763dd13fc9158b2a7d05e820ed
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-07-17 01:06:55 +0000

    Adjust wsgi application settings again

--- power_standz/settings.py
+++ power_standz/settings.py
@@ -114,7 +114,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'powerstandz.wsgi.application'
+WSGI_APPLICATION = 'power_standz.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
