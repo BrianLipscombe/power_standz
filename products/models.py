0c4bf3a1627214f5e769f7040cb4fc4ec39e41ef
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-07-17 20:49:33 +0000

    Undo remove categories model due to heroku app error

--- products/models.py
+++ products/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
