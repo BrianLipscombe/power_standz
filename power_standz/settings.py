96c553f28398b1c663b5b23584189b62736cfe6d
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-07-17 23:00:01 +0000

    shopping bag refactor

--- power_standz/settings.py
+++ power_standz/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
