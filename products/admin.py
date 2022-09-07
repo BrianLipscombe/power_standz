11113fb113b38812788d275873717a643b59cf16
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-09-07 19:13:32 +0000

    Fix internal server error when opening app in Heroku

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product, Category
 
+# Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -14,13 +15,11 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
-
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
