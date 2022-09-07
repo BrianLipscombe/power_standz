92771a43f633949afe3d817720532ae5f8e38999
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-09-07 18:58:40 +0000

    Fix problems

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -14,6 +13,7 @@ class ProductAdmin(admin.ModelAdmin):
     )
 
     ordering = ('sku',)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
