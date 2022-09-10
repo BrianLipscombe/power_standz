e75897128827e2ad7130f61af6945621205f7b24
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-09-10 02:17:25 +0000

    using S3StaticStorage for media and static

--- custom_storages.py
+++ custom_storages.py
@@ -6,5 +6,5 @@ class StaticStorage(S3StaticStorage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3Boto3Storage):
+class MediaStorage(S3StaticStorage):
     location = settings.MEDIAFILES_LOCATION
