184a07319cb52a639391d540a143bece8e5fb34a
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-09-10 02:26:25 +0000

    Reverted media storage to Boto3Storage

--- custom_storages.py
+++ custom_storages.py
@@ -6,5 +6,5 @@ class StaticStorage(S3StaticStorage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3StaticStorage):
+class MediaStorage(S3Boto3Storage()):
     location = settings.MEDIAFILES_LOCATION
