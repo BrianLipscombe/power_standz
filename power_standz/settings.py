91e5dbb3c4df7045fde6aef2dbb898e7583239eb
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-09-15 18:19:49 +0000

    Set debug to true for testing

--- power_standz/settings.py
+++ power_standz/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['powerstandz.herokuapp.com', 'localhost']
 
