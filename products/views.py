0a953d01a387b571ac5b8007c744ce1bfadf7144
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-09-23 01:03:17 +0000

    Add a view to show product detail

--- products/views.py
+++ products/views.py
@@ -59,12 +59,16 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
+from reviews.forms import ReviewForm
+
+
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = Product.objects.select_related().get(pk=product_id)
 
     context = {
+        'review_form': ReviewForm(),
         'product': product
     }
 
